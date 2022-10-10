5418c2c74a1362ff4dff99ca4668fb284039b3bf
David Li <david_li@college.harvard.edu>
2022-10-09 22:46:04 -0700

    .

---
+++ db_explorer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python -i
+
+from lib_cl import *
+
+ROW_SZ = 40
+
+e = Emailer()
+lp = LexProcessor(rowsize=ROW_SZ)
+pdb = PostDb()
+imdb = ImDb()
+ldb = LexDb(row_size=ROW_SZ)
+dbs = [pdb,imdb,ldb]
