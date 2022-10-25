cf255b6e7dc74e078d0b3b7f2634ad5574998172
David Li <david_li@college.harvard.edu>
2022-10-25 01:34:25 -0700

    .

--- cl_runner.py
+++ cl_runner.py
@@ -14,7 +14,7 @@ crypto_mining = ['the rig', 'power consumption']
 vending_grams = ['pressed steel', 'used and refurbished', 'are for quarters','machine is', 'machine will', 'soda vending', 'vending machine']
 coin_sorter = ['coin sorter', 'operating modes','power source', 'fast sort','for each denomination', 'money jar']
 arcade = ['coins or freeplay', 'arcade game','arcade games', 'slot machine', 'insert coin', 'arcade cabinet',
-          'work on', 'the machine', 'cherry coins', 'beautiful cabinet', 'on coins', 'refurbished with',
+          'work on', 'the machine', 'cherry coins', 'beautiful cabinet', 'refurbished with',
           'coinoperated','coinop', 'coin op', 'coin operated']
 
 banned_ngrams_raw = cash_for_coins+piggy_bank_grams+wallet_grams+crypto_mining+vending_grams+coin_sorter+arcade
